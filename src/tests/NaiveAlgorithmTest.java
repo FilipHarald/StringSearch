a1293a55838a3e6a37ad4c866691459fe34c8a5e
FilipHarald <Filip.Harald@gmail.com>
2015-11-21 14:14:17 +0100

    added alphabet as testfile

---
+++ src/tests/NaiveAlgorithmTest.java
@@ -0,0 +1,14 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class NaiveAlgorithmTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
