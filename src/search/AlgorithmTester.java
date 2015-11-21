4527764b1dd510adaa655d6d3a57d5c26d902d62
nekosaur <albert@kaaman.nu>
2015-11-21 12:22:25 +0100

    Added Main method, method outputs

--- src/search/AlgorithmTester.java
+++ src/search/AlgorithmTester.java
@@ -1,19 +1,28 @@
 package search;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
 public class AlgorithmTester {
 
-	public static List<SearchResult> run(List<Algorithm> algorithms, TestData data, int times) {
+	public static void run(List<Algorithm> algorithms, TestData data, int times) {
 		
 		List<SearchResult> results = new LinkedList<>();
 		
+		System.out.println("AlgorithmTester");
+		System.out.println("T = " + Arrays.toString(data.getT()));
+		System.out.println("P = " + Arrays.toString(data.getP()));
+		System.out.println();
+		
 		for (Algorithm algorithm : algorithms) {
+			System.out.println("Running algorithm " + algorithm.getClass().getSimpleName() + " over data " + times + " times");
 			results.add(Search.run(algorithm, data, times));
 		}
 		
+		for (SearchResult result : results) {
+			System.out.println(result);
+		}
 		
-		return null;
 	}
 }
