af8308c2c52c0b837b65f6c244829bb1b45ad231
FilipHarald <Filip.Harald@gmail.com>
2015-11-21 14:02:04 +0100

    Implemented reading from file

--- src/search/entities/TestData.java
+++ src/search/entities/TestData.java
@@ -1,9 +1,17 @@
 package search.entities;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.function.Consumer;
+
 public class TestData {
 	private final char[] p;
 	private final char[] t;
 	
+	public static void main(String[] args) {
+		load("lol");
+	}
 	public TestData(char[] t, char[] p) {
 		this.p = p;
 		this.t = t;
@@ -16,4 +24,23 @@ public class TestData {
 	public char[] getT() {
 		return t;
 	}
+	
+	public static TestData loadFiles(String filename){
+		return new TestData(load(filename + ".t"), load(filename + ".p"));
+	}
+	
+	public static TestData loadFiles(String tFilename, String pFilename){
+		return new TestData(load(tFilename + ".t"), load(pFilename + ".p"));
+	}
+	
+	public static char[] load(String filename){
+		final StringBuilder sb = new StringBuilder();
+		try {
+			Files.lines(Paths.get("resources", filename)).forEachOrdered(s -> sb.append(s));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return sb.toString().toCharArray();
+		}
+	
 }
