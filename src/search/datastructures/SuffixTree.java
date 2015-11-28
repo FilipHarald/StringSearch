97f73bee617a9cb05de19c949fa6f98f39efadc4
nekosaur <albert@kaaman.nu>
2015-11-28 17:07:09 +0100

    Fixed problem with empty edges. Added method to save graph as image

--- src/search/datastructures/SuffixTree.java
+++ src/search/datastructures/SuffixTree.java
@@ -1,10 +1,20 @@
 package search.datastructures;
 
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Random;
 
-import com.sun.javafx.geom.Edge;
+import com.mxgraph.layout.hierarchical.mxHierarchicalLayout;
+import com.mxgraph.layout.mxIGraphLayout;
+import com.mxgraph.util.mxCellRenderer;
+import org.jgrapht.ext.JGraphModelAdapter;
+import org.jgrapht.ext.JGraphXAdapter;
+import org.jgrapht.graph.DefaultDirectedGraph;
+
+import javax.imageio.ImageIO;
 
 public class SuffixTree {
 	private final char[] text;
@@ -23,14 +33,17 @@ public class SuffixTree {
 		for (char c : text) {
 			System.out.println("/////////////////////////////////////////////////////////////////////////////////////////////////////////");
 			this.add(c, remainder, activePoint, null);
-			System.out.println(toString());
 			length++;
+			System.out.println(toString());
 		}
 		
 	}
 	
 	private void add(char c, int remainder, ActivePoint activePoint, SuffixNode prevSplitNode) {
-		
+
+		System.out.println(activePoint);
+		System.out.println("Adding " + c);
+
 		char temp;
 		if (activePoint.edgeFirstChar != '\0') {
 			temp = activePoint.edgeFirstChar;
@@ -49,7 +62,7 @@ public class SuffixTree {
 					activePoint.edgeFirstChar = c;
 				}
 				activePoint.length++;
-				System.out.println("current char: " + c + " length++");
+				System.out.println("length++");
 				remainder++;
 				
 				if (activePoint.length == edge.length) {
@@ -60,7 +73,7 @@ public class SuffixTree {
 				
 			} else {
 				
-				edge.length = activePoint.length - 1;
+				edge.length = edge.textIndex + activePoint.length - 1;
 				
 				SuffixEdge newEdge = new SuffixEdge(new SuffixNode(), edge.textIndex + activePoint.length, -1);
 				SuffixEdge newEdge2 = new SuffixEdge(new SuffixNode(), length, -1);
@@ -76,7 +89,7 @@ public class SuffixTree {
 						activePoint.edgeFirstChar = c;
 					else
 						activePoint.edgeFirstChar = text[edge.textIndex + 1];
-					System.out.println("current char: " + c + " length--");
+					System.out.println("length--");
 				} else {
 					activePoint.node = activePoint.node.suffixLink != null ? activePoint.node.suffixLink : root;
 				}
@@ -98,7 +111,33 @@ public class SuffixTree {
 		}
 		
 	}
-	
+
+	public void saveAsImage() {
+		DefaultDirectedGraph<SuffixNode, SuffixEdge> g = new DefaultDirectedGraph<SuffixNode, SuffixEdge>(SuffixEdge.class);
+
+		for (Map.Entry<SuffixKey, SuffixEdge> entry : hashMap.entrySet()) {
+			SuffixNode start = entry.getKey().node;
+			SuffixNode end = entry.getValue().endNode;
+
+			g.addVertex(start);
+			g.addVertex(end);
+			g.addEdge(start, end, entry.getValue());
+		}
+
+		JGraphXAdapter<SuffixNode, SuffixEdge> jgxa = new JGraphXAdapter<SuffixNode, SuffixEdge>(g);
+
+		mxIGraphLayout asd = new mxHierarchicalLayout(jgxa);
+
+		asd.execute(jgxa.getDefaultParent());
+
+		BufferedImage bi = mxCellRenderer.createBufferedImage(jgxa, null, 1, Color.WHITE, true, null);
+
+		try {
+			ImageIO.write(bi, "PNG", new File("graph.png"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 	
 	@Override
 	public String toString() {
@@ -112,13 +151,15 @@ public class SuffixTree {
 		return sb.toString();
 	}
 
-
-
 	private class SuffixEdge {
-		private final SuffixNode endNode;
+		private SuffixNode endNode;
 		private int textIndex;
 		private int length;
-		
+
+		public SuffixEdge() {
+			super();
+		}
+
 		public SuffixEdge(SuffixNode endNode, int index, int length) {
 			this.endNode = endNode;
 			this.textIndex = index;
@@ -128,11 +169,12 @@ public class SuffixTree {
 		@Override
 		public String toString() {
 			StringBuilder sb = new StringBuilder();
-			int end = length < 0 ? SuffixTree.this.length : length;
-			for (int i = textIndex; i <= end ; i++)
+			int end = length < 0 ? SuffixTree.this.length : length + 1;
+			for (int i = textIndex; i < end ; i++)
 				sb.append(text[i]);
-			
+
 			return "SuffixEdge [endNode=" + endNode + ", textIndex=" + textIndex + ", length=" + length + ", text=" + sb.toString() + "]";
+			//return sb.toString();
 		}
 				
 	}
@@ -151,6 +193,7 @@ public class SuffixTree {
 
 		@Override
 		public String toString() {
+			//return "";
 			return Integer.toString(hashCode());
 		}
 		
@@ -169,6 +212,14 @@ public class SuffixTree {
 			this.length = length;
 		}
 
+		@Override
+		public String toString() {
+			return "ActivePoint{" +
+					"node=" + node +
+					", edgeFirstChar=" + edgeFirstChar +
+					", length=" + length +
+					'}';
+		}
 	}
 	
 	private class SuffixKey {
@@ -209,7 +260,7 @@ public class SuffixTree {
 			return true;
 		}
 
-		
+
 	}
 }
 
--- src/tests/TestSuffixTree.java
+++ src/tests/TestSuffixTree.java
@@ -10,6 +10,10 @@ public class TestSuffixTree {
 		SuffixTree st = new SuffixTree("abcabxabcd".toCharArray());
 		
 		// Den tomma noden 1284693 -> SuffixEdge [endNode=17225372, textIndex=1, length=0, text=] är den som ska var b, har fått length 0
-		
+		// Detta borde vara fixat!
+
+		// Är något problem som får nod 1414644648 att hoppa av trädet i steg 7-9 nånstans
+
+		st.saveAsImage();
 	}
 }
