174fc59d38d3d8380d126901c29e8f7391965595
nekosaur <albert@kaaman.nu>
2015-12-01 18:02:42 +0100

    Lots of stuff, not all good

--- src/search/datastructures/SuffixTree.java
+++ src/search/datastructures/SuffixTree.java
@@ -7,10 +7,12 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Map;
+import java.util.Set;
 import java.util.List;
 
-import com.mxgraph.layout.hierarchical.mxHierarchicalLayout;
-import com.mxgraph.layout.mxIGraphLayout;
+import com.mxgraph.layout.hierarchical.*;
+import com.mxgraph.layout.orthogonal.mxOrthogonalLayout;
+import com.mxgraph.layout.*;
 import com.mxgraph.util.mxCellRenderer;
 import org.jgrapht.ext.JGraphModelAdapter;
 import org.jgrapht.ext.JGraphXAdapter;
@@ -35,10 +37,11 @@ public class SuffixTree {
 		for (char c : text) {
 			System.out.println("/////////////////////////////////////////////////////////////////////////////////////////////////////////");
 			this.add(c, remainder, activePoint, null);
+			this.saveAsImage(length);
 			length++;
 			System.out.println("Length = " + length);
-			System.out.println(toString());
-			this.saveAsImage(length);
+			//System.out.println(toString());
+			
 		}
 		
 	}
@@ -55,14 +58,15 @@ public class SuffixTree {
 			temp = c;
 		}
 		
-		SuffixEdge edge = hashMap.get(new SuffixKey(activePoint.node, temp));
+		//SuffixEdge edge = hashMap.get(new SuffixKey(activePoint.node, temp));
+		SuffixEdge edge = activePoint.node.hashMap.get(temp);
 		
-		System.out.println("found edge = " + edge);
+//		System.out.println("found edge = " + edge);
 		
 		if (edge != null) {
 			
 			if (text[edge.textIndex + activePoint.length] == c) {
-				System.out.println("Found c on current edge");
+//				System.out.println("Found char on current edge");
 				
 				if (temp == c) {					
 					activePoint.edgeFirstChar = c;
@@ -71,21 +75,32 @@ public class SuffixTree {
 				System.out.println("length++");
 				remainder++;
 				
-				if (activePoint.length == edge.length + 1) {
+				if (activePoint.length == edge.length) { // + 1
 					activePoint.node = edge.endNode;
 					activePoint.edgeFirstChar = '\0';
 					activePoint.length = 0;
 				}
 				
 			} else {
-				System.out.println("Splitting edge");
-				edge.length = edge.textIndex + activePoint.length - 1;
+				System.out.println("Splitting edge " + edge);
+				edge.length = activePoint.length; /*edge.textIndex + activePoint.length - 1; */
+				
+				System.out.println("New current edge = " + edge);
 				
 				SuffixEdge newEdge = new SuffixEdge(new SuffixNode(), edge.textIndex + activePoint.length, -1);
 				SuffixEdge newEdge2 = new SuffixEdge(new SuffixNode(), length, -1);
 				
-				hashMap.put(new SuffixKey(edge.endNode, text[edge.textIndex + activePoint.length]), newEdge);
-				hashMap.put(new SuffixKey(edge.endNode, c), newEdge2);
+				System.out.println("New edge 1 = " + newEdge);
+				System.out.println("New edge 2 = " + newEdge2);
+				
+				for (Map.Entry<Character, SuffixEdge> child : edge.endNode.hashMap.entrySet()) {
+					child.getValue().textIndex -= 1;
+				}
+				
+				//hashMap.put(new SuffixKey(edge.endNode, text[edge.textIndex + activePoint.length]), newEdge);
+				edge.endNode.hashMap.put(text[edge.textIndex + activePoint.length], newEdge);
+				//hashMap.put(new SuffixKey(edge.endNode, c), newEdge2);
+				edge.endNode.hashMap.put(c, newEdge2);
 				
 				remainder--;
 				
@@ -94,7 +109,7 @@ public class SuffixTree {
 					if (activePoint.length == 0)
 						activePoint.edgeFirstChar = c;
 					else
-						activePoint.edgeFirstChar = text[edge.textIndex + 1];
+						activePoint.edgeFirstChar = text[edge.textIndex + 1]; // + 1
 					System.out.println("length--");
 				} else {
 					activePoint.node = activePoint.node.suffixLink != null ? activePoint.node.suffixLink : root;
@@ -109,13 +124,30 @@ public class SuffixTree {
 			
 		} else {
 			System.out.println("got here");
-			edge = new SuffixEdge(new SuffixNode(), length, -1);
 			
-			hashMap.put(new SuffixKey(root, c), edge);
+			if (!activePoint.node.equals(root))
+				activePoint.node.hashMap.put(c, new SuffixEdge(new SuffixNode(), length, -1));
+				//hashMap.put(new SuffixKey(activePoint.node, c), new SuffixEdge(new SuffixNode(), length, -1));
+			
+			//hashMap.put(new SuffixKey(root, c), new SuffixEdge(new SuffixNode(), length, -1));
+			root.hashMap.put(c, new SuffixEdge(new SuffixNode(), length, -1));
+			
 			
 			activePoint.edgeFirstChar = '\0';
+			activePoint.node = root;
 		}
 		
+	}
+	
+	private void findLeaves(List<Integer> matches, SuffixNode node, int currentLength) {
+		System.out.println(node);
+		for (Map.Entry<Character, SuffixEdge> entry : node.hashMap.entrySet()) {
+			if (entry.getValue().endNode.hashMap.size() <= 0) {
+				matches.add(entry.getValue().textIndex - currentLength);
+			} else {
+				findLeaves(matches, entry.getValue().endNode, currentLength + entry.getValue().length);
+			}
+		}		
 	}
 	
 	public List<Integer> find(String pattern) {
@@ -126,35 +158,85 @@ public class SuffixTree {
 		int currentLength = 0;
 		
 		while (node != null) {
-			
-			SuffixEdge edge = hashMap.get(new SuffixKey(node, pattern.charAt(currentIndex)));
-			
-			if (edge != null) {
-				SuffixNode nextNode = edge.endNode;
-				for (int i = currentIndex + 1; i < edge.length; i++)
-					if (text[edge.textIndex + i] != pattern.charAt(i))
-						nextNode = null;
-				node = nextNode;
-				currentIndex = currentIndex + edge.length;
+			System.out.println(node);
+			if (currentLength == pattern.length()) {
+				System.out.println("here");
+				// If we've gotten to a node and currentLength is equal to pattern length, then all leaf nodes starting from this node are matching suffixes to pattern
+				/*
+				int suffixLength = currentLength;
+				for (Map.Entry<Character, SuffixEdge> entry : node.hashMap.entrySet()) {
+					SuffixNode endNode = entry.getValue().endNode;
+					
+					if (endNode.hashMap.size() > 0) {
+						suffixLength += entry.getValue().length;
+					} else {
+						suffixLength -= 
+						matches.add(entry.getValue().textIndex - suffixLength);
+					}	
+				}
+				*/
+				findLeaves(matches, node, currentLength);
+				
+				return matches;
+				
+			} else {
+				//SuffixEdge edge = hashMap.get(new SuffixKey(node, pattern.charAt(currentIndex)));			
+				SuffixEdge edge = node.hashMap.get(pattern.charAt(currentIndex));
+				System.out.println(edge);
+				SuffixNode nextNode = null;
+				if (edge != null) {
+					nextNode = edge.endNode;
+					int end = edge.length < 0 ? text.length - edge.textIndex : edge.length;
+					for (int i = 0; i < end; i++) {
+						if (currentIndex + i >= pattern.length()) {
+							matches.add(edge.textIndex - (i - currentIndex) - 1);
+							return matches;  // single match since we finished on an edge
+						} else if (text[edge.textIndex + i] != pattern.charAt(currentIndex + i)) {
+							return matches; // matches is empty at this point
+						}
+					}
+					currentLength += edge.length;
+					node = nextNode;
+					currentIndex = currentIndex + edge.length;
+				}
 			}
+			
+		}
+		return null;
+	}
+	
+	private void addEdges(DefaultDirectedGraph<SuffixNode, SuffixEdge> g, SuffixNode n) {
+//		if (!g.containsVertex(n))
+//			g.addVertex(n);
+		for (Map.Entry<Character, SuffixEdge> child : n.hashMap.entrySet()) {
+			g.addVertex(child.getValue().endNode);
+			g.addEdge(n, child.getValue().endNode, child.getValue());
+			addEdges(g, child.getValue().endNode);
 		}
 	}
 
 	public void saveAsImage(int length) {
 		DefaultDirectedGraph<SuffixNode, SuffixEdge> g = new DefaultDirectedGraph<SuffixNode, SuffixEdge>(SuffixEdge.class);
 
+		/*
 		for (Map.Entry<SuffixKey, SuffixEdge> entry : hashMap.entrySet()) {
 			SuffixNode start = entry.getKey().node;
 			SuffixNode end = entry.getValue().endNode;
 
-			g.addVertex(start);
-			g.addVertex(end);
+			if (!g.containsVertex(start))
+				g.addVertex(start);
+			if (!g.containsVertex(end))
+				g.addVertex(end);
 			g.addEdge(start, end, entry.getValue());
-		}
-
+		}*/
+		
+		g.addVertex(root);
+		addEdges(g, root);
+		
 		JGraphXAdapter<SuffixNode, SuffixEdge> jgxa = new JGraphXAdapter<SuffixNode, SuffixEdge>(g);
 
 		mxIGraphLayout asd = new mxHierarchicalLayout(jgxa);
+		//mxIGraphLayout asd = new mxCompactTreeLayout(jgxa);	
 
 		asd.execute(jgxa.getDefaultParent());
 
@@ -197,10 +279,12 @@ public class SuffixTree {
 		@Override
 		public String toString() {
 			StringBuilder sb = new StringBuilder();
-			int end = length < 0 ? SuffixTree.this.length : length + 1;
+			
+			int end = length < 0 ? SuffixTree.this.length + 1 : textIndex + length;
 			for (int i = textIndex; i < end ; i++)
 				sb.append(text[i]);
-
+			
+			sb.append("["+textIndex+","+length+"]");
 			//return "SuffixEdge [endNode=" + endNode + ", textIndex=" + textIndex + ", length=" + length + ", text=" + sb.toString() + "]";
 			return sb.toString();
 		}
@@ -210,6 +294,7 @@ public class SuffixTree {
 	private class SuffixNode {
 		private SuffixNode suffixLink;
 		private int id;
+		private final Map<Character, SuffixEdge> hashMap = new HashMap<>();
 
 		public SuffixNode(int id) {
 			this.id = id;
--- src/tests/TestSuffixTree.java
+++ src/tests/TestSuffixTree.java
@@ -1,19 +1,33 @@
 package tests;
 
+import java.util.List;
+
 import search.datastructures.SuffixTree;
+import search.entities.TestData;
 
 public class TestSuffixTree {
 
 	
 	public static void main(String[] args) {
 		
-		SuffixTree st = new SuffixTree("abcabxabcd".toCharArray());
+		TestData data = TestData.loadFiles("alphabet");
+		
+		//SuffixTree st = new SuffixTree("abcabxabcd$".toCharArray());
+		SuffixTree st = new SuffixTree(data.getT());
 		
 		// Den tomma noden 1284693 -> SuffixEdge [endNode=17225372, textIndex=1, length=0, text=] är den som ska var b, har fått length 0
 		// Detta borde vara fixat!
 
 		// Är något problem som får nod 1414644648 att hoppa av trädet i steg 7-9 nånstans
 
-		//st.saveAsImage();
+		//st.saveAsImage(1);
+		
+		
+		/*
+		List<Integer> matches = st.find("jo");
+		System.out.println("Matches=");
+		for (int m : matches)
+			System.out.println(m);
+		*/
 	}
 }
