5671f21a8a9cf94d726be4201da5aa38e95354f1
FilipHarald <Filip.Harald@gmail.com>
2015-12-02 13:56:28 +0100

    changed tests to use the preprocessmethod

--- src/tests/NaiveAlgorithmTest.java
+++ src/tests/NaiveAlgorithmTest.java
@@ -34,7 +34,8 @@ public class NaiveAlgorithmTest extends TestCase {
 	@Test
 	public void testBible() {
 		data = TestData.loadFiles("Bible-en");
-		result = Search.run(algorithm, data, 1);
+		algorithm.preProcess(data.getT());
+		result = algorithm.run(data.getP());
 		assertTrue("First occurrance of Jesus", result.getMatches().get(0) == 3067567);
 		assertTrue("Number of times Jesus is mentioned", result.getMatches().size() == 977);
 	}
